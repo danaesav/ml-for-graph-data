4c44e3007fcc6da66309fdc33ef9e75d9618026d
thegirlwiththesquirrel <d.n.savvidi@student.tudelft.nl>
2025-05-31 22:37:25 +0800

    eval part

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
+import torch
 import torch.nn as nn
 from torch import optim
 from torch_geometric_temporal import temporal_signal_split
 import numpy as np
-from tqdm import tqdm
 
+from sklearn.metrics import f1_score
 from GeneratorDatasetLoader import GeneratorDatasetLoader
 from MultiLabelEvolveGCN import MultiLabelEvolveGCN
 from temporal_multi_label_generator import TemporalMultiLabelGeneratorConfig
@@ -36,15 +37,45 @@ loss_fn = nn.BCEWithLogitsLoss()
 # ---------------------
 
 model.train()
-for epoch in tqdm(range(200)):
+for epoch in range(200):
     total_loss = 0
     for snapshot in train_dataset:
         optimizer.zero_grad()
         y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
         loss = loss_fn(y_hat, snapshot.y)
-        loss.backward(retain_graph=True)
+        loss.backward()
         optimizer.step()
         total_loss += loss.item()
-    # avg_loss = total_loss / (time + 1) ?
-    # if epoch % 20 == 0:
-        print(f"Epoch {epoch} | Loss: {total_loss:.4f}")
+    avg_loss = total_loss / train_dataset.snapshot_count
+    print(f"Epoch {epoch} | Loss: {avg_loss:.4f}")
+
+
+# ---------------------
+# EVALUATION
+# ---------------------
+model.eval()
+total_loss = 0
+all_preds = []
+all_targets = []
+
+with torch.no_grad():
+    for snapshot in test_dataset:
+        y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+        loss = loss_fn(y_hat, snapshot.y)
+        total_loss += loss.item()
+
+        # Apply sigmoid and threshold to get predictions
+        probs = torch.sigmoid(y_hat)
+        preds = (probs > 0.5).float() #the threshold ig you can play with it
+
+        all_preds.append(preds.cpu())
+        all_targets.append(snapshot.y.cpu())
+
+avg_loss = total_loss / test_dataset.snapshot_count
+
+# Concatenate all batches
+all_preds = torch.cat(all_preds, dim=0).numpy()
+all_targets = torch.cat(all_targets, dim=0).numpy()
+
+val_f1 = f1_score(all_targets, all_preds, average='macro') # macro means compute per label and then average
+print(f"Test BCE Loss: {avg_loss:.4f} | Validation Macro F1 Score: {val_f1:.4f}")
